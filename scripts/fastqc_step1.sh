cb6dfcd8bb832ace2bca17e38f55f0c25884432a
Liz Rebboah <erebboah@uci.edu>
2021-03-09 07:01:56 -0800

    added scripts

--- scripts/fastqc_step1.sh
+++ scripts/fastqc_step1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=fastqc    ## Name of the job
+#SBATCH --job-name=checkreads    ## Name of the job
 #SBATCH -A SEYEDAM_LAB            ## account to charge 
 #SBATCH -p standard               ## partition/queue name
 #SBATCH --nodes=1                 ## (-N) number of nodes to use
 #SBATCH --array=1-5               ## number of tasks to launch (number of samples)
 #SBATCH --cpus-per-task=1         ## number of cores the job needs
-#SBATCH --output=fastqc-%J.out ## output log file
-#SBATCH --error=fastqc-%J.err ## error log file
+#SBATCH --output=checkreads-%J.out ## output log file
+#SBATCH --error=checkreads-%J.err ## error log file
 
 inpath="/share/crsp/lab/seyedam/share/newATAC/"
 file=$inpath"prefixes_all.txt" # sample prefix file containing the names for the samples you want to process
